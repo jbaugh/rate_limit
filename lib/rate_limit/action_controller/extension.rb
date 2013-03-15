1f2cdc90d96e59c8508f719079580f08e5bbbd4b
Jarrett Baugh <jarrett.baugh@gmail.com>
2013-03-14 23:45:02 -0700

    Some changes to how the rate limiter works

--- lib/rate_limit/action_controller/extension.rb
+++ lib/rate_limit/action_controller/extension.rb
@@ -2,42 +2,49 @@ module RateLimit
   module ActionController
     module Extension
 
+      def self.included(base)
+        base.send(:include, InstanceMethods)
+        base.extend(ClassMethods)
+      end
+
       # USAGE:
       # limit :index, :to => 10, :within => 60
 
-      def self.limit(action,options={})
-        # uncommment to force explicit settings
-        # raise 'number of requests (:to) must be specified' unless options[:to].present?
-        # raise 'rate limit interval (:within) must be specified' unless options[:within].present?
+      module ClassMethods
+        def limit(action, options={})
+          raise 'number of requests (:to) must be specified' unless options[:to].present?
+          raise 'rate limit interval (:within) must be specified' unless options[:within].present?
 
-        limiter = Rails3Limiter.new
-        limiter.config.threshold = options[:to] if options[:to].present?
-        limiter.config.interval  = options[:within] if options[:within].present?
+          limiter = Rails3Limiter.new
+          
+          options.each do |opt, val| 
+            limiter.config.send("#{opt}=", val)
+          end
 
-        options.slice(options.keys-[:to,:within]).each { |o| limiter.config.send "#{o}=", options[o] }
+          config.limiters                = {} unless config.limiters.present?
+          config.limiters[action.to_sym] = limiter
 
-        config.limiters                = {} unless config.limiters.present?
-        config.limiters[action.to_sym] = limiter
-
-        before_filter :rate_limit_filter, :only => action
+          before_filter :rate_limit_filter, :only => action
+        end
       end
 
-      def rate_limit_filter
-        limiter = config.limiters[params[:action].to_sym]
-        rate_limit_reached! if limiter.present? && limiter.reached_limit?(*rate_limit_args)
-      end
+      module InstanceMethods
+        def rate_limit_filter
+          limiter = config.limiters[params[:action].to_sym]
+          rate_limit_reached! if limiter.present? && limiter.reached_limit?(*rate_limit_args)
+        end
 
-      def rate_limit_args
-        params[:controller], params[:action], request.method, request.remote_ip
-      end
+        def rate_limit_args
+          return params[:controller], params[:action], request.method, request.remote_ip
+        end
 
-      def rate_limit_reached!
-        render :text => 'Too Many Requests', :status => 400
+        def rate_limit_reached!
+          render :text => 'Too Many Requests', :status => 400
+        end
       end
 
     end
   end
 end
 
-# extend ActionController
-ActionController::Base.include RateLimit::ActionController::Extension
+ActionController::Base.send :include, RateLimit::ActionController::Extension
--- lib/rate_limit/rails_3_limiter.rb
+++ lib/rate_limit/rails_3_limiter.rb
@@ -12,34 +12,53 @@ module RateLimit
 
     def initialize
       config.cache = Rails.cache unless config.cache.present?
+      config.logger = Rails.logger unless config.logger.present?
     end
 
-    def reached_limit?(controller,action,method,ip)
+    def reached_limit?(controller, action, method, ip)
       return false if whitelisted_ip?(ip)
 
-      key       = cache_key controller, action, method, ip
+      key       = cache_key(controller, action, method, ip)
       attempts  = config.cache.read(key).to_i                 # nil.to_i => 0
       attempts += 1                                           # increment by one
 
-      config.cache.write key, attempts, :expires_in => config.interval.to_i
+      config.cache.write(key, attempts, :expires_in => config.interval.to_i)
 
-      attempts <= config.threshold
+      if attempts > config.threshold
+        lockout!
+        true
+      else
+        false
+      end
     rescue => e
-      log e.message
-      log e.backtrace.join("\n")
+      config.logger.info e.message
+      config.logger.info e.backtrace.join("\n")
       true
+    end
+
+    def clear!
+      config.logger.info "Calling clear! for #{self.cache_key}"
+      config.cache.delete(self.cache_key)
     end
     
     protected
+
+    def self.lockout_value
+      'LOCKED'
+    end
+
+    def lockout!
+      config.logger.info "Calling lockout! for #{self.cache_key}"
+      config.cache.write(self.cache_key, self.class.lockout_value, config.lockout_period)
+    end
     
     # TODO have this work for IP ranges as well, see:
     # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/ipaddr/rdoc/IPAddr.html
-
     def whitelisted_ip?(ip)
       config.allowed_ips.include?(ip)
     end
     
-    def cache_key(controller,action,method,ip)
+    def cache_key(controller, action, method, ip)
       request = "#{controller}##{action}"
       request = "#{method.upcase}_#{request}" if config.limit_by_method
       "rate_limit/requests/#{ip}/#{request}"
